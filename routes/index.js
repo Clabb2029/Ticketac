7da96ea26416037bf955b4518805f79ce80c596f
Mathieu Pereira <mathieu.pereira&live.fr>
2022-01-27 10:43:19 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var options = {
  };
 
 // --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
+mongoose.connect('mongodb+srv://admin:ticktac@ticketac.focws.mongodb.net/Ticketac?retryWrites=true&w=majority',
    options,
    function(err) {
     if (err) {
