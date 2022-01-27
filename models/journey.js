efdfe5fee599d70d41c57bc27af3842893fce40a
Clara Slys <slys.clara89000@gmail.com>
2022-01-27 10:32:05 +0100

    models

---
+++ models/journey.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+var journeySchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: Date,
+    departureTime: String,
+    price: Number,
+  });
+  
+  var journeyModel = mongoose.model('journey', journeySchema);
